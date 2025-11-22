cacb4320656a3ca64cfb0a07313a3ece25c24dae
Naren7874 <malinarendra7874@gmail.com>
2025-11-22 15:13:40 +0530

    admin dashboad

--- server/routes/dashboardRoutes.js
+++ server/routes/dashboardRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getDashboardStats, getDashboardGraphData } = require('../controllers/dashboardController');
+const { getDashboardStats, getDashboardGraphData, getDashboardPieChartData } = require('../controllers/dashboardController');
 const { authenticateToken } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.get('/', authenticateToken, getDashboardStats);
 router.get('/graph', authenticateToken, getDashboardGraphData);
+router.get('/pie-chart', authenticateToken, getDashboardPieChartData);
 
 module.exports = router;
